563088caff8d538a9de9be4c78c26f38b66c8257
mohikhsan <mohikhsan@Mohammads-MacBook-Pro.local>
2017-03-25 14:11:34 +0700

    Added trainer folders

---
+++ ai-core/trainer/training_core.py
@@ -0,0 +1,25 @@
+import numpy as np
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.layers.convolutional import Convolution1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+def training(trainX, trainY, testX, testY):
+    """Trains LSTM net using updated datasets
+
+    """
+
+    print("Training Cuy")
+
+
+def update_train_db(inputFile):
+    """Update training database
+
+    """
+
+    
---
+++ ai-core/trainer/training_main.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+import sys, getopt
+import json
+import training_core as tc
+
+def main(argv):
+    tc.update_train_db(input_query)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
