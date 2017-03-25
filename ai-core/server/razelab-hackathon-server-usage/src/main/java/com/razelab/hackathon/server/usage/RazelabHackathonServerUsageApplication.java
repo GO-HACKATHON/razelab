f45ea6acaf3e731c3071cf17949bcd45a06d9549
Firman <firmanazhari@gmail.com>
2017-03-26 02:00:07 +0800

    add usage-engine webserver

---
+++ ai-core/server/razelab-hackathon-server-usage/src/main/java/com/razelab/hackathon/server/usage/RazelabHackathonServerUsageApplication.java
@@ -0,0 +1,12 @@
+package com.razelab.hackathon.server.usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazelabHackathonServerUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazelabHackathonServerUsageApplication.class, args);
+	}
+}
